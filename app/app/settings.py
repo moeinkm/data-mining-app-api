23485df0cbb70a777dca049044c054e5721800ae
moein <moein.kameli.mk@gmail.com>
2022-07-14 16:17:45 +0430

    debug False

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8#=jt+*m76t!y&ak+o&$u%j4c5wx8jk^j+9nt^+@eayefxg+pc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'preprocess-data-app-api.herokuapp.com'
